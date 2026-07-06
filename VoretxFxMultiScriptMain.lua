cbace31fcd8cd89c985ae9b567b913624237ebb5
Vortex-FX-source <sourovsamadder2@gmail.com>
2026-07-06 15:48:32 +0530

    Update VoretxFxMultiScriptMain.lua

--- VoretxFxMultiScriptMain.lua
+++ VoretxFxMultiScriptMain.lua
@@ -10,17 +10,13 @@ print("🌪️ Vortex FX: Scanning game...")
 -- [ 1. BLOX FRUITS (Sea 1, Sea 2, and Sea 3) ] --
 if PlaceId == 2753915549 or PlaceId == 4442272183 or PlaceId == 7449423635 or PlaceId == 7447361652 then
     print("Vortex FX: Blox Fruits Detected! Loading Hub...")
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Vortex-FX-source/Vortex-Hub/refs/heads/main/Vortex-FX"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/flazhy/QuantumOnyx/refs/heads/main/QuantumOnyx.lua"))()
 
 -- [ 2. ANIME CARD COLLECTION ] --
 elseif PlaceId == 76285745979410 then
     print("Vortex FX: Anime Card Collection Detected! Loading Hub...")
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Vortex-FX-source/Vortex-Hub/refs/heads/main/VortexAnimeCardsCollection"))()
 
--- [ 3. WAIFU CARDS COLLECTION ] --
-elseif PlaceId == 9952351777 then
-    print("Vortex FX: Waifu Cards Detected! Loading Hub...")
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Vortex-FX-source/Vortex-Hub/refs/heads/main/AnimeWifuCardCollection"))()
 
 -- [ 4. EVERY OTHER GAME (Universal Fly Script) ] --
 else
